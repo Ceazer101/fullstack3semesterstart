5cf7530a0c4de209ce83198f2804cd74fffe2106
chris <chri402s@stud.kea.dk>
2022-08-24 12:56:29 +0200

    Initial commit

---
+++ src/main/java/com/example/fullstack3semesterstart/Fullstack3semesterstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstack3semesterstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fullstack3semesterstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fullstack3semesterstartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstack3semesterstart/Fullstack3semesterstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstack3semesterstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fullstack3semesterstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
